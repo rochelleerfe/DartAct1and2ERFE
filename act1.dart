30eadae889faded36210bb5204adf4482c66c849
rochelleerfe <erferochelle0104@gmail.com>
2025-08-07 09:46:31 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ act1.dart
@@ -0,0 +1,13 @@
+void main() {
+String greeting = "Welcome";
+String user = "Alice";
+
+print("$greeting, ${user}!"); // The Output: Welcome, Alice
+}
+
+void main() {
+String greeting = "Welcome";
+dynamic user = 25;
+
+print("$greeting, ${user}!"); // The Output: Welcome, 25
+}
